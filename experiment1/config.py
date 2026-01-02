22c972e6ab4ccf1c5251e6df959d1a1c09671a6d
sbrunswi <sabine.brunswicker@gmail.com>
2026-01-02 11:07:12 -0500

    updated store_trajectories script

--- experiment1/config.py
+++ experiment1/config.py
@@ -1,5 +1,4 @@
-
-def get_configs(num_exp, max_step=1):
+def get_configs(num_exp, max_step=1,device='cpu'):
 
     # get sub exp configs
     num_past = num_exp
@@ -7,7 +6,7 @@ def get_configs(num_exp, max_step=1):
     move_penalty = -0.01
 
     env_kwargs = dict(height=11, width=11, pixel_per_grid=8, preference=100, exp=1, save=True)
-    model_kwargs = dict(num_past=num_past, num_input=11, device='cuda')
+    model_kwargs = dict(num_past=num_past, num_input=11, device=device)
     exp_kwargs = dict(num_past=num_past, num_step=num_step, move_penalty=move_penalty)
     agent_kwargs = dict(agent_type='random')
 
